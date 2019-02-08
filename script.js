d2123e6865b5360920b8aa6c0d7a20345ed3200f
neldane137 <47365976+neldane137@users.noreply.github.com>
2019-02-07 20:36:24 -0800

    assignment 1
    gh

---
+++ script.js
@@ -0,0 +1,188 @@
+/*eslint-env browser */
+
+//STEP 1
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+
+//STEP 2
+10				// numeric literal 
+"Hello World!" 	// string literal 
+true 			// Boolean literal 
+null 			// literal null value 
+
+//STEP 3
+var totalScore = quizPoints + assignmentPoints;
+var percentGrade = totalScore * 100 / totalAvailablePoints;
+
+
+//STEP 4
+var strFirstName;
+var strLastName;
+var strAddress;
+var strCity;
+var strState;
+var numZipCode; 
+var numYourAge;
+var strReferralSource;
+var blnMayWeContactYou;
+
+//STEP 5
+var strFirstName = "Nelda";
+var strFirstName;
+strFirstName = "Nelda";
+
+var strFirstName = "Nelda", var strLastName = "Danetaras";
+
+var strAddress = "8530 Aero Dr", var strCity = "San Diego", var strState = "CA", var strZipCode = 92123;  
+
+var numYourAge = "20";
+var strReferralSource = "School";
+var blnMayWeContactYou = true;
+
+
+//STEP 6
+
+var yourAge;
+yourAge= 4 + "ten";
+window.console.log("She is " + yourAge + " years old.");
+
+
+//STEP 7
+var booleanString = true + "True";
+window.console.log("Display " + booleanString + ".");
+
+var numberBoolean = 5 + true;
+window.console.log("Display result is six " + numberBoolean + ".");
+
+
+//STEP 8
+//original string was invalid. The correct string is below:
+var someString = "Who once said, 'Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.'";
+window.console.log(someString);
+
+
+//STEP 9
+var x = 5;
+var y = null;
+x = null;
+window.console.log(x);
+
+
+var valueUndef;
+window.console.log(valueUndef);
+
+
+//STEP 10   string, number, Boolean, object, and undefined.
+typeof "JavaScript"
+typeof 100
+typeof true
+typeof [a,b,c]
+typeof typeOf
+
+
+//STEP 11
+window.alert("Hello " + "Nelda Danetaras," + " welcome to the JavaScript class!");
+
+
+//STEP 12
+var name = "Nelda Danetaras";
+window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+
+//STEP 13
+var name = "Nelda Danetaras";
+var course = "JavaScript";
+window.alert("Hello " + name + ", welcome to the " + course + " class!");
+
+
+//STEP 14
+var greeting = "Hello Zak Ruvalcaba.\nWelcome to the JavaScript class!";
+window.alert(greeting);
+
+//STEP 15
+var name = prompt ("What is your name?");
+window.alert("Hello " + name + ", welcome to the JavaScript class!");
+
+//STEP 16
+var name = prompt ("What is your name?");
+var course = prompt ("What is your course?");
+window.alert("Hello " + name + ", welcome to the " + course + " class!");
+
+
+// STEP 17
+x = 10;
+y = 20;
+window.console.log(x+y);
+
+
+// STEP 18
+var x = 20;
+x += 20;
+window.console.log(x);
+
+
+
+// STEP 19
+var x = 20;
+x *= 5;
+window.console.log(x);
+
+
+// STEP 20
+var x = 20 % 3;
+window.console.log(x);
+
+// STEP 21
+var x = 2;
+var y = 10;
+window.console.log(x <= y != false);
+
+
+
+// STEP 22
+var x = 12;
+var y = -8;
+window.console.log(x > 0 && y > 0);
+
+
+// STEP 23
+var z = 100;
+var widget = z++;
+window.console.log(typeof widget);
+
+
+
+// STEP 24
+var string = new String();
+window.console.log(instructor instanceof String);
+
+
+// STEP 25
+var string = new String();
+window.console.log(instructor instanceof Number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
